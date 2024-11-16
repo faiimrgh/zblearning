8e841563fe8af16c6312a4d4418c6b45768e1834
unknown <faiimr@gmail.com>
2024-11-16 14:43:04 +0300

    third commit

--- AzureWebApps/obj/Debug/net8.0/AzureWebApps.AssemblyInfo.cs
+++ AzureWebApps/obj/Debug/net8.0/AzureWebApps.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AzureWebApps")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+927a5723c8a5324ab3e089810c5b95b534dc8ba5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("AzureWebApps")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AzureWebApps")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
